e7e35e67b0972b7ae96bae0c780fe3d4ac408965
iAcay <96024046+iAcay@users.noreply.github.com>
2022-05-25 20:42:07 +0200

    feat: introduce possibility to create messages from UI

--- app/controllers/chatroom_controller.rb
+++ app/controllers/chatroom_controller.rb
@@ -1,6 +1,8 @@
 class ChatroomController < ApplicationController
+  before_action :require_user
 
   def index
+    @message = Message.new
     render :index, locals: { messages: Message.order('created_at ASC').includes(:user) }
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -12,11 +12,14 @@
 
 ActiveRecord::Schema.define(version: 2022_05_25_092510) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "messages", force: :cascade do |t|
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
@@ -30,4 +33,5 @@ ActiveRecord::Schema.define(version: 2022_05_25_092510) do
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
+  add_foreign_key "messages", "users"
 end
