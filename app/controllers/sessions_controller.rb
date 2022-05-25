dc1b03e70f25be2becc3843ffd3e4a4e40853137
iAcay <96024046+iAcay@users.noreply.github.com>
2022-05-25 20:41:08 +0200

    fix: change alerts/notices to messages

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,24 +1,32 @@
 class SessionsController < ApplicationController
+  before_action :logged_in_redirect, only: [:new, :create]
 
-  def new
-
-  end
+  def new; end
 
   def create
     user = User.find_by(username: params[:session][:username])
     if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
-      flash[:notice] = "Welcome! You have been succesfully logged in."
+      flash[:success] = "Welcome! You have been succesfully logged in."
       redirect_to root_path
     else
-      flash.now[:alert] = "Wrong username or password."
+      flash.now[:error] = "Wrong username or password."
       render :new
     end
   end
 
   def destroy
     session[:user_id] = nil
-    flash[:notice] = "Bye! You have been succesfully logged out."
+    flash[:success] = "Bye! You have been succesfully logged out."
     redirect_to login_path
+  end
+
+  private
+
+  def logged_in_redirect
+    if logged_in?
+      flash[:error] = "You are already logged in."
+      redirect_to root_path
+    end
   end
 end
